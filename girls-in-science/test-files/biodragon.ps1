c97ed13e3109b164785c3b7fe9b001386de5be97
Abhinav Sharma <abhi18av@gmail.com>
2017-09-09 01:53:35 +0530

    working on the proper shell  scripts

--- girls-in-science/test-files/biodragon.ps1
+++ girls-in-science/test-files/biodragon.ps1
@@ -1,9 +1,29 @@
 Set-Location /Users/eklavya/Projects/ProjectEklavya/Eklavya/Code/BioDragon/girls-in-science/test-files
 
 
+$allFileNames =  Get-ChildItem -Name -Filter "*fastq*"
+
+#  Get-ChildItem -Name -Filter "*LRu*.*R1*"
+
+$allGenomeNames = @()
+foreach ($fileName in $allFileNames) {
+                $allGenomeNames += $fileName.Split("_")[6]
+
+}
+
+$uniqueGenomeNames =  $allGenomeNames | Select-Object -Unique
+
+
+$RCodes = @("R1", "R2")
+
+
 
 # print out proper genome name 
+
 # print out the corresponding R-string 
+
+
+
 
 ## generate the corresponding name for the output-file
 
@@ -12,7 +32,6 @@ $arr = Get-ChildItem -Filter *R1* -Name
 
 foreach ($a in $arr) {
                 Get-Content $a | Out-File -Append -NoNewline out.txt
-                #Write-Host $a
 
 }
 
@@ -20,5 +39,3 @@ foreach ($a in $arr) {
 
 
 
-
-get-help Add-Content
