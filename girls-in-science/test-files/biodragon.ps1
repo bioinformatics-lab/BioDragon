c889ebd50fe137a8340352fa578ffc0dc268b879
Abhinav Sharma <abhi18av@gmail.com>
2017-09-09 01:24:26 +0530

    working on a proper solution

--- girls-in-science/test-files/biodragon.ps1
+++ girls-in-science/test-files/biodragon.ps1
@@ -1,10 +1,24 @@
+Set-Location /Users/eklavya/Projects/ProjectEklavya/Eklavya/Code/BioDragon/girls-in-science/test-files
+
+
+
+# print out proper genome name 
+# print out the corresponding R-string 
+
+## generate the corresponding name for the output-file
+
 $arr = Get-ChildItem -Filter *R1* -Name
 
 
 foreach ($a in $arr) {
-                cat $a | Set-Content out.txt
+                Get-Content $a | Out-File -Append -NoNewline out.txt
                 #Write-Host $a
 
 }
+
+
+
+
+
 
 get-help Add-Content
