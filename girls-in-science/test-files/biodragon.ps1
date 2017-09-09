757ae592f24cc126cbbb7bfc4a0661f2ea41ebe6
Abhinav Sharma <abhi18av@gmail.com>
2017-09-09 14:18:45 +0530

    added the checks on the function parameters

--- girls-in-science/test-files/biodragon.ps1
+++ girls-in-science/test-files/biodragon.ps1
@@ -5,10 +5,11 @@ I'm assuming that you'd be using << Windows >> here
 How to use this powershell script:
 
 1. Save this file in the << EAI Genome_Emilyn >> folder - along with the basic genome files to be merged.
+( NOTE: Make sure of this with << Get-Location >> )
 
 2. Move to the directory using << terminal >>
 
-3. Next, type << ./biodragon.ps1 >>
+3. Next, type << biodragon.ps1 >>
 
 4. You should see the desired files merged and saved properly
 
