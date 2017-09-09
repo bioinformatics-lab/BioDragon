91e83565d9c03e643201601599f7b86e821eb6e9
Abhinav Sharma <abhi18av@gmail.com>
2017-09-09 13:10:54 +0530

    the bioDragon is coming along just fine!

--- girls-in-science/test-files/biodragon.ps1
+++ girls-in-science/test-files/biodragon.ps1
@@ -17,33 +17,49 @@ How to use this powershell script:
 
 # First we generate a list of all files whose names end with << fastq >> 
 
-$allFastqFiles =  Get-ChildItem -Name -Filter "*fastq*" 
+$allFastqFiles = Get-ChildItem -Name -Filter "*fastq*" 
 
 
 function findUniqueGenomes {
-
-
-
-                Write-Host $allFastqFiles
-
-<# 
+ 
                 # initialize an empty array
-$allGenomeNames = @()
+                $allGenomeNames = @()
 
-foreach ($fileName in $allFastqFiles) {
-                $allGenomeNames += $fileName.Split("_")[6]
-
-} 
-                return $allGenomeNames              
-
-
- #>
+                foreach ($fileName in $allFastqFiles) {
+                                $allGenomeNames += $fileName.Split("_")[6]
+                } 
+                return $allGenomeNames | Select-Object -Unique
 
 }
 
 
 
+# Here we call the << findUniqueFenomes >> function to store the unique genomes in another array.
+$uniqueGenomeNames = findUniqueGenomes 
 
-$uniqueGenomeNames =  $allGenomeNames | Select-Object -Unique
 
+
+function allFilesForAGenome($genome) {
+                $genomeRegExp = "*" + $genome + "*" 
+                Get-ChildItem -Name  -Filter $genomeRegExp
+
+}
+
+
+# A function that returns the list of << R1 >> files associated with a genome
+
+function combineAllR2ForGenome ($genome ) {
+
+
+                foreach ($genome in $uniqueGenomeNames) {
+                                if ($genome.Contains("_R2_")) {
+                                                #Write-Host $genome 
+                                                Get-Content $genome | Out-File -Append -NoNewline $file
+
+                                }
+                }
+
+}
+
+makeFastqR2 ("biodragon-r2.txt")
 
