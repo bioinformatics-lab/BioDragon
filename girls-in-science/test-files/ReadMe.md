b18240784f03bf728e6ad18c0ae11aaa27f5d142
Abhinav Sharma <abhi18av@gmail.com>
2017-09-10 07:04:13 +0530

    started working on the ruby version

--- girls-in-science/test-files/ReadMe.md
+++ girls-in-science/test-files/ReadMe.md
@@ -1,8 +1,19 @@
-
+### How to use the script
 
 A simple usage can be checked from here
 https://vimeo.com/233094262
 
+
+
+
+
+### 10 September 2017
+
+
+
+
+
+### 9 September 2017
 The `script` is located here
 
 https://github.com/abhi18av/BioDragon/blob/master/girls-in-science/test-files/biodragon.ps1
---
+++ girls-in-science/test-files/biodragon.rb
@@ -0,0 +1,81 @@
+# How to use this powershell script:
+
+# 1. Save this file in the << EAI Genome_Emilyn >> folder - along with the basic genome files to be merged.
+# ( NOTE: Make sure of this with << Get-Location >> )
+
+# 2. Move to the directory using << terminal >>
+
+# 3. Next, type << biodragon.rb >>
+
+# 4. You should see the desired files merged and saved properly
+
+
+
+# First we generate a list of all files whose names end with << fastq >>
+$allFastqFiles = Get-ChildItem -Name -Filter "*fastq*"
+
+# This function finds the names of the unique Genomes within the folder
+function findUniqueGenomes {
+
+                # initialize an empty array
+                $allGenomeNames = @()
+
+                foreach ($fileName in $allFastqFiles) {
+                                $allGenomeNames += $fileName.Split("_")[6]
+                }
+                return $allGenomeNames | Select-Object -Unique
+
+}
+
+
+
+# Generates the associated << FASTQ >> files for a particular genome
+function allFilesForAGenome {
+
+                Param(
+                                [parameter(Mandatory = $true)] [String] $genome
+
+
+                )
+
+                $genomeRegExp = "*_" + $genome + "_*"
+                Get-ChildItem -Name  -Filter $genomeRegExp
+
+}
+
+
+# A function that returns the list of << R1 >> files associated with a genome
+function combineAllRCodeFilesForGenome {
+                Param(
+                                [parameter(Mandatory = $true, Position = 0)] [String] $genome,
+
+                                [parameter(Mandatory = $true, Position = 1)] [String] $RCode
+
+                )
+
+                $outputFileName = ($genome).ToString() + "_" + $RCode + ".fastq"
+                $filesForAGenome = allFilesForAGenome($genome)
+                $RCodeFilesForAGenome = $filesForAGenome -like "*_" + $RCode + "_*"
+
+
+                foreach ($anRCodeFile in $RCodeFilesForAGenome) {
+                                Get-Content $anRCodeFile | Out-File -Append -NoNewline -Encoding ASCII $outputFileName
+
+                }
+}
+
+
+
+# Here we call the << findUniqueFenomes >> function to store the unique genomes in another array.
+$uniqueGenomeNames = findUniqueGenomes
+
+
+# Show time, baby!
+foreach ($genome in $uniqueGenomeNames) {
+                Write-Host "Working on the $genome files"
+                combineAllRCodeFilesForGenome $genome "R1"
+                combineAllRCodeFilesForGenome $genome "R2"
+}
+
+
+Write-Host "All Done"
