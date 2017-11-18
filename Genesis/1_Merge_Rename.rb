c2f127a4c66691ee0cc28144e4d9b0ec77a00de1
Abhinav Sharma <abhi18av@gmail.com>
2017-11-18 19:23:15 +0530

    added the UglyFigures as submodule

---
+++ Genesis/1_Merge_Rename.rb
@@ -0,0 +1,90 @@
+# How to use this ruby script:
+
+# 1. Save this file in the << EAI Genome_Emilyn >> folder - along with the basic genome files to be merged.
+# ( NOTE: Make sure of this with << pwd >> )
+
+# 2. Move to the directory using << terminal >>
+
+# 3. Next, type << ruby biodragon.rb >>
+
+# 4. You should see the desired files merged and saved properly
+
+# First we generate a list of all files whose names end with << fastq >>
+
+allFastqFiles = Dir["*.fastq"]
+
+# Usage
+#findUniqueGenomes(allFastqFiles)
+
+def findUniqueGenomes(allFastqFiles)
+  allGenomeList = []
+
+  for fileName in allFastqFiles
+    genomeName = fileName.split("_")[6]
+
+    allGenomeList.push(genomeName)
+  end
+
+  uniqueGenomeList = (allGenomeList.uniq).compact
+  return uniqueGenomeList
+end
+
+# Usage
+# allFilesForAGenome("G04868")
+def allFilesForAGenome (genome)
+  genomeRegExp = "*_" + genome + "_*"
+
+  return Dir[genomeRegExp]
+end
+
+# Usage
+# combineAllRCodeFilesForGenome("G04868","R1")
+def combineAllRCodeFilesForGenome(genome, rCode)
+  outputFileName = (genome).to_s + "_" + rCode + ".fastq"
+
+  filesForAGenome = allFilesForAGenome(genome)
+
+  rCodeFilesForAGenome = []
+
+  for f in filesForAGenome
+    rPart = f.split("_")[-2]
+
+    if rPart == rCode
+      rCodeFilesForAGenome.push(f)
+    end
+  end
+
+  #return rCodeFilesForAGenome
+
+  # construct the string to be executed by the shell
+  cmd = "cat "
+  # combine the names of the files into a single string
+  for file in rCodeFilesForAGenome
+    cmd += file + " "
+  end
+
+  cmd += " > #{outputFileName}"
+  puts cmd
+  system(cmd)
+end
+
+# Here we call the << findUniqueGenomes >> function to store the unique genomes in another array.
+uniqueGenomeList = findUniqueGenomes(allFastqFiles)
+
+
+# Show time baby!
+# Calling the << combineAllRCodeFilesForGenome >> functions per genome for both << R >> files
+# The << puts >> is used for a well informed user experience while running the script
+for genome in uniqueGenomeList
+  puts "\n\n ~~~~~~~~~~~~~~~~~~~~~ "
+  puts "\nWorking on the #{genome} files\n\n"
+  puts "\n>>>>       R1      <<<<\n\n"
+  combineAllRCodeFilesForGenome(genome,"R1")
+  puts "\n\n"
+  puts "\n>>>>       R2      <<<<\n\n"
+  combineAllRCodeFilesForGenome(genome,"R2")
+end
+
+puts "\n\n\n@@@@@@@@@@@@@@@@@@@@@@@@"
+
+puts "\n\nAll Done!"
