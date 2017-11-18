926f0f43cfa80d691bcf0266b49113c8b0767b84
Abhinav Sharma <abhi18av@gmail.com>
2017-11-18 22:31:11 +0530

    working on the genesis.scala script

--- Genesis/genesis.scala
+++ Genesis/genesis.scala
@@ -1,3 +1,19 @@
 import ammonite.ops._
 import ammonite.ops.ImplicitWd._
 
+// exists! home/'BioDragon
+
+val wd = home/'BioDragon
+
+var all_files = ls! wd/'course_files
+
+all_files.foreach{println}
+
+
+// val reference_genome = NC000962_3.fasta
+
+ls! pwd/up
+
+time{ls!}
+
+
