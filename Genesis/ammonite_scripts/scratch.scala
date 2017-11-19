5fa9f89a4a2145622ec6db5a730ebcf5e39dedf3
Abhinav Sharma <abhi18av@gmail.com>
2017-11-19 14:23:00 +0530

    working on the scratch to get to know ammonite better

--- Genesis/ammonite_scripts/scratch.scala
+++ Genesis/ammonite_scripts/scratch.scala
@@ -4,3 +4,32 @@ import ammonite.ops._
 import ammonite.ops.ImplicitWd._
 
 
+%("ls" , "-l")
+ls!
+
+
+%("pwd")
+pwd
+
+
+// exists! home/'BioDragon
+
+val wd = home/'BioDragon
+
+var all_files = ls! wd/'course_files
+
+all_files.foreach{println}
+
+
+// val reference_genome = NC000962_3.fasta
+
+ls! pwd/up
+
+
+
+mkdir! 'test
+
+rm! pwd/'test
+
+
+write("hello.txt", "hello")
