c4f87a5fd18b96d7ad8bf839a9afa6f90897be72
Abhinav Sharma <abhi18av@gmail.com>
2017-11-19 19:28:15 +0530

    working on a proper ammonite script package

--- Genesis/ammonite_scripts/genesis_main.scala
+++ Genesis/ammonite_scripts/genesis_main.scala
@@ -1,16 +1,3 @@
-// exists! home/'BioDragon
-
-val wd = home/'BioDragon
-
-var all_files = ls! wd/'course_files
-
-all_files.foreach{println}
-
-
-// val reference_genome = NC000962_3.fasta
-
-ls! pwd/up
-
 
 ////////////////////////////////////////////////
 // EXERCISE - 1
